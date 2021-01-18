94088a52a25764977c9f13735621901ec03ab70d
Salman-Inayat <salmaninayat1999@gmail.com>
2021-01-18 22:20:32 +0500

    initial files uploaded

---
+++ includes/footer.php
@@ -0,0 +1,116 @@
+<footer id="footer" class="footer color-bg" style="background-color: #666;">
+    <div class="links-social inner-top-sm" >
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <!-- ============================================================= CONTACT INFO ============================================================= -->
+                    <div class="contact-info">
+                        <div class="footer-logo">
+                            <div class="logo">
+                                <a href="index.php">
+
+                                    <h3>Atec Mart</h3>
+                                </a>
+                            </div><!-- /.logo -->
+                        </div>
+                        <div class="module-body m-t-20">
+                            <p class="about-us"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur scelerisque diam et orci mattis accumsan. Vestibulum consequat dolor non tellus cursus maximus. Donec euismod faucibus dolor, et scelerisque nulla pharetra bibendum. Quisque porttitor, leo eu commodo placerat</p>
+                        </div>
+
+                    </div><!-- /.contact-info -->
+                </div><!-- /.col -->
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class="contact-timing">
+                        <div class="module-heading">
+                            <h4 class="module-title" style="color: white">opening time</h4>
+                        </div><!-- /.module-heading -->
+
+                        <div class="module-body outer-top-xs">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <tbody>
+                                        <tr>
+                                            <td>Monday-Friday:</td>
+                                            <td class="pull-right">08.00 To 18.00</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Saturday:</td>
+                                            <td class="pull-right">09.00 To 20.00</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Sunday:</td>
+                                            <td class="pull-right">10.00 To 20.00</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div><!-- /.table-responsive -->
+                        </div><!-- /.module-body -->
+                    </div><!-- /.contact-timing -->
+                    <!-- ============================================================= CONTACT TIMING : END ============================================================= -->
+                </div><!-- /.col -->
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <!-- ============================================================= INFORMATION============================================================= -->
+                    <div class="contact-information">
+                        <div class="module-heading">
+                            <h4 class="module-title" style="color: white">information</h4>
+                        </div><!-- /.module-heading -->
+
+                        <div class="module-body outer-top-xs">
+                            <ul class="toggle-footer" style="">
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack">
+                                            <i class="fa fa-stack-2x"></i>
+                                            <i class="fa fa-map-marker fa-stack-1x fa-2x"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p>Main Office, I9, Islamabad</p>
+                                    </div>
+                                </li>
+
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack ">
+                                            <i class="fa  fa-stack-2x"></i>
+                                            <i class="fa fa-mobile fa-stack-1x fa-2x"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p>(+92) 345 6606603</p>
+                                    </div>
+                                </li>
+
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack "> 
+                                            <i class="fa fa-stack-2x"></i>
+                                            <i class="fa fa-envelope fa-stack-1x fa-2x"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <span><a href="#">atecmart@gmail.com</a></span>
+                                    </div>
+                                </li>
+
+                            </ul>
+                            <!-- <div class="social-icons">
+
+<a href="http://facebook.com/transvelo" class='active'><i
+        class="icon fa fa-facebook"></i></a>
+<a href="#"><i class="icon fa fa-twitter"></i></a>
+<a href="#"><i class="icon fa fa-linkedin"></i></a>
+<a href="#"><i class="icon fa fa-rss"></i></a>
+<a href="#"><i class="icon fa fa-pinterest"></i></a>
+
+</div> -->
+                        </div><!-- /.module-body -->
+                    </div><!-- /.contact-timing -->
+         
+                    <!-- ============================================================= INFORMATION : END ============================================================= -->
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
---
+++ includes/main-header.php
@@ -0,0 +1,150 @@
+<?php 
+
+ if(isset($_Get['action'])){
+		if(!empty($_SESSION['cart'])){
+		foreach($_POST['quantity'] as $key => $val){
+			if($val==0){
+				unset($_SESSION['cart'][$key]);
+			}else{
+				$_SESSION['cart'][$key]['quantity']=$val;
+			}
+		}
+		}
+	}
+?>
+<div class="main-header" style="background-color: currentColor">
+    <div class="container">
+        <div class="row" >
+            <div class="col-xs-12 col-sm-12 col-md-3 logo-holder" style="margin-top: 0px">
+                <div class="logo">
+                    <a href="index.php">
+                        <h2 style="margin-top: 15px">Atec Mart</h2>
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-5 ">
+                <div class="yamm navbar" role="navigation" style="margin-top:5px">
+                    <div class="navbar-header">
+                        <button data-target="#mc-horizontal-menu-collapse" data-toggle="collapse"
+                            class="navbar-toggle collapsed" type="button">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="nav-bg-class">
+                        <div class="navbar-collapse collapse" id="mc-horizontal-menu-collapse">
+                            <div class="nav-outer">
+                                <ul class="nav navbar-nav">
+                                    <li class=" dropdown yamm-fw ">
+                                        <a href="index.php" data-hover="dropdown" id="menu-link" class="dropdown-toggle">Home</a>
+                                    </li>
+                                    <li class=" dropdown yamm-fw">
+                                        <a href="index.php" data-hover="dropdown" class="dropdown-toggle">About Us</a>
+                                    </li>                            
+                                    <li class=" dropdown yamm-fw">
+                                        <a href="index.php" data-hover="dropdown" class="dropdown-toggle">Contact US</a>
+                                    </li>                            
+                                    <li class=" dropdown yamm-fw">
+                                        <a href="index.php" data-hover="dropdown" class="dropdown-toggle">Privacy Policy</a>
+                                    </li>
+                                </ul>
+                                <div class="clearfix"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3 top-search-holder">
+                <div class="search-area" style="border-radius: 5px; border: 1px solid seagreen;">
+                    <form name="search" method="post" action="search-result.php">
+                        <div class="control-group" style="position: relative">
+                            <input class="search-field" style="width: 100%; border-radius: 5px" placeholder="Search here..." name="product"
+                                required="required"/>
+                            <button class="btn btn-primary" style="height: 100%; position: absolute; right: 0px; top: 0px;" type="submit" name="search"><span><i class="fa fa-search fa-2"></i></span></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-1 animate-dropdown top-cart-row">
+                <?php
+if(!empty($_SESSION['cart'])){
+	?>
+                <div class="dropdown dropdown-cart">
+                    <a href="#" class="dropdown-toggle lnk-cart" data-toggle="dropdown">
+                        <div class="items-cart-inner">
+                            <div class="basket">
+                                <i class="glyphicon glyphicon-shopping-cart"></i>
+                            </div>
+                            <div class="basket-item-count"><span class="count"><?php echo $_SESSION['qnty'];?></span>
+                            </div>
+                        </div>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <?php
+    $sql = "SELECT * FROM products WHERE id IN(";
+			foreach($_SESSION['cart'] as $id => $value){
+			$sql .=$id. ",";
+			}
+			$sql=substr($sql,0,-1) . ") ORDER BY id ASC";
+			$query = mysqli_query($con,$sql);
+			$totalprice=0;
+			$totalqunty=0;
+			if(!empty($query)){
+			while($row = mysqli_fetch_array($query)){
+				$quantity=$_SESSION['cart'][$row['id']]['quantity'];
+				$subtotal= $_SESSION['cart'][$row['id']]['quantity']*$row['productPrice']+$row['shippingCharge'];
+				$totalprice += $subtotal;
+				$_SESSION['qnty']=$totalqunty+=$quantity;?>
+                        <li>
+                            <div class="cart-item product-summary">
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <h3 class="name"><a
+                                                href="product-details.php?pid=<?php echo $row['id'];?>"><?php echo $row['productName']; ?></a>
+                                        </h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php } }?>
+                            <hr>
+                        </li>
+                    </ul>
+                </div>
+                <?php } else { ?>
+                <div class="dropdown dropdown-cart">
+                    <a href="#" class="dropdown-toggle lnk-cart" data-toggle="dropdown">
+                        <div class="items-cart-inner">
+                            <div class="basket" style="border-radius: 5px">
+                                <i class="glyphicon glyphicon-shopping-cart"></i>
+                            </div>
+                            <div class="basket-item-count"><span class="count">0</span></div>
+
+                        </div>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <div class="cart-item product-summary">
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        Your Shopping Cart is Empty.
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="clearfix cart-total">
+
+                                <div class="clearfix"></div>
+
+                                <a href="index.php" class="btn btn-upper btn-primary btn-block m-t-20">Continue
+                                    Shopping</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <?php }?>
+            </div>
+        </div>
+    </div>
+</div>

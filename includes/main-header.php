4593b08822c38c085f819d3b10a062f964ca84e0
Salman-Inayat <salmaninayat1999@gmail.com>
2021-01-25 10:41:11 +0500

    more changes done=

--- includes/main-header.php
+++ includes/main-header.php
@@ -21,7 +21,7 @@
 <div class="main-header" style="background-color: #f44336">
     <div class="container">
         <div class="row" >
-            <div class="col-xs-12 col-sm-12 col-md-3 logo-holder" style="margin-top: 0px; color: white">
+            <div class="col-xs-12 col-sm-12 col-md-4 logo-holder" style="margin-top: 0px; color: white">
                 <div class="logo">
                     <a href="index.php">
                         <h2 style="margin-top: 15px; color: white">Atec Mart</h2>
@@ -50,13 +50,12 @@
                                         <a href="about.php" data-hover="dropdown" class="nav-links dropdown-toggle" style="color: white">About Us</a>
                                     </li>                            
                                     <li class=" dropdown ">
-                                        <a href="contact.php" data-hover="dropdown" class="nav-links dropdown-toggle" style="color: white">Contact US</a>
+                                        <a href="contact.php" data-hover="dropdown" class="nav-links dropdown-toggle" style="color: white">Contact Us</a>
                                     </li>                            
                                     <li class=" dropdown ">
                                         <a href="privacy.php" data-hover="dropdown" class="nav-links dropdown-toggle" style="color: white">Privacy Policy</a>
                                     </li>
                                 </ul>
-                                <div class="clearfix"></div>
                             </div>
                         </div>
                     </div>
@@ -72,74 +71,6 @@
                         </div>
                     </form>
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-1 animate-dropdown top-cart-row">
-                <?php
-if(!empty($_SESSION['cart'])){?>
-                <div class="dropdown dropdown-cart">
-                    <a href="#" class="dropdown-toggle lnk-cart" data-toggle="dropdown">
-                        <div class="items-cart-inner">
-                            <div class="basket">
-                                <i class="glyphicon glyphicon-shopping-cart"></i>
-                            </div>
-                            <div class="basket-item-count"><span class="count"><?php echo $_SESSION['qnty'];?></span>
-                            </div>
-                        </div>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <?php
-    $sql = "SELECT * FROM products WHERE id IN(";
-			foreach($_SESSION['cart'] as $id => $value){
-			$sql .=$id. ",";
-			}
-			$sql=substr($sql,0,-1) . ") ORDER BY id ASC";
-			$query = mysqli_query($con,$sql);
-			$totalprice=0;
-			$totalqunty=0;
-			if(!empty($query)){
-			while($row = mysqli_fetch_array($query)){
-				$quantity=$_SESSION['cart'][$row['id']]['quantity'];
-				$subtotal= $_SESSION['cart'][$row['id']]['quantity']*$row['productPrice']+$row['shippingCharge'];
-				$totalprice += $subtotal;
-				$_SESSION['qnty']=$totalqunty+=$quantity;?>
-                        <li>
-                            <div class="cart-item product-summary">
-                                <div class="row">
-                                    <div class="col-xs-12">
-                                        <h3 class="name"><a
-                                                href="product-details.php?pid=<?php echo $row['id'];?>"><?php echo $row['productName']; ?></a>
-                                        </h3>
-                                    </div>
-                                </div>
-                            </div>
-                            <?php } }?>
-                            <hr>
-                        </li>
-                    </ul>
-                </div>
-                <?php } else { ?>
-                <div class="dropdown dropdown-cart">
-                    <a href="#" class="dropdown-toggle lnk-cart" data-toggle="dropdown">
-                        <div class="items-cart-inner">
-                            <div class="basket" style="border-radius: 5px">
-                                <i class="glyphicon glyphicon-shopping-cart"></i>
-                            </div>
-                            <div class="basket-item-count"><span class="count">0</span></div>
-                        </div>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <div class="cart-item product-summary">
-                                <div class="row">
-                                    <div class="col-xs-12">
-                                        Your Shopping Cart is Empty.
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <?php }?>
             </div>
         </div>
     </div>
